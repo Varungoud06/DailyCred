74ae849b1fd4a9b3d72ffbc5e3bddafdc29ac219
unknown <Varungoud06>
2026-04-09 11:25:45 +0530

    Initial commit

---
+++ Dailycred/src/test/java/com/unqiuehire/kashflow/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unqiuehire.kashflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
